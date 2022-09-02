26894b6d02bf7f13c4bb8fa8fb2419b28b01cd17
zhuxianglong29 <992167551@qq.com>
2022-09-02 13:36:28 +0800

    第第二次提交

--- DFRobot_ZXL.cpp
+++ DFRobot_ZXL.cpp
@@ -1,21 +1,20 @@
-#include <sensor.h>  
+#include <DFRobot_ZXL.h>  
 
 
 
 
 
 
-DFRobot_Sensor::DFRobot_Sensor(uint8_t mode)
+DFRobot_ZXL::DFRobot_ZXL(uint8_t mode)
 :_mode(*((uint8_t*)&mode))   //知识点：C++中构造函数才有的初始化列表，可以进行变量的初始化，这里的功能是初始化_mode
 {
-	
+
   
 }
-int DFRobot_Sensor::begin(void)
+int DFRobot_ZXL::begin(void)
 {
   uint8_t id=0xDF;
-  uint8_t buf;
-  if(readReg(SENSOR_ADDR_ID,&buf,1) == 0){//#define SENSOR_ADDR_ID     4 //芯片ID寄存器地址 这里的描述从芯片手册上抄写 ，
+  if(readReg(SENSOR_ADDR_ID,&id,1) == 0){//#define SENSOR_ADDR_ID     4 //芯片ID寄存器地址 这里的描述从芯片手册上抄写 ，//readReg用于多类继承
   //readReg的具体功能见下文
     DBG("bus data access error");
     return ERR_DATA_BUS;  //#define ERR_DATA_BUS      -1      //数据总线错误
@@ -24,19 +23,31 @@ int DFRobot_Sensor::begin(void)
   if(id !=DFRobot_Sensor_ID){
     return ERR_IC_VERSION;  //#define ERR_IC_VERSION    -2      //芯片版本不匹配
   }
-  writeReg(SENSOR_ADDR_CONFIG,&_mode,1); //向配置寄存器的地址写入要设置的模式，初始化从机
+  writeReg(SENSOR_ADDR_CONFIG,&_mode,1); //向配置寄存器的地址写入要设置的模式，初始化从机//
   DBG("begin ok!");
   return ERR_OK;  //#define ERR_OK             0      //无错误
 }
-uint16_t DFRobot_Sensor::soundStrengthDB(void)
+
+uint16_t DFRobot_ZXL::soundStrengthDB(void)
 {
   sCombinedData_t data;  //data这个结构体变量占一个字节，根据手册上这个寄存器定义的，前4位存放声音强度，后4位存放光线强度
   readReg(SENSOR_ADDR_DATA, &data, sizeof(data));  //从SENSOR_ADDR_DATA（组合数据地址）读数据，存放到data中，所以传的是data的地址
   DBG("sound reg raw data is");
   DBG(data.sound);  
-  return data.sound << 3; //单位是DB，所以要移位？？
+  return data.sound << 3; //单位是DB，所以要移位，数据要扩大8倍是真实数据？？
 }
-uint32_t DFRobot_Sensor::lightStrengthLux(void)
+
+/* uint16_t DFRobot_Sensor::soundStrengthDB(void)
+{
+  uint8_t data;  //data这个结构体变量占一个字节，根据手册上这个寄存器定义的，前4位存放声音强度，后4位存放光线强度
+  readReg(SENSOR_ADDR_DATA, &data, sizeof(data));  //从SENSOR_ADDR_DATA（组合数据地址）读数据，存放到data中，所以传的是data的地址
+  DBG("sound reg raw data is");
+  DBG(data>>4);  
+  return (data>>4)<<3; //单位是DB，所以要移位，数据要扩大8倍是真实数据？？
+}
+ */
+ 
+uint32_t DFRobot_ZXL::lightStrengthLux(void)
 {
   sCombinedData_t data;
   readReg(SENSOR_ADDR_DATA, &data, sizeof(data));
@@ -44,7 +55,8 @@ uint32_t DFRobot_Sensor::lightStrengthLux(void)
   DBG(data.light);
   return data.light * 10000;
 }
-uint8_t DFRobot_Sensor::switchMode(uint8_t mode)
+
+uint8_t DFRobot_ZXL::switchMode(uint8_t mode)
 {
   uint8_t tmp;
   #ifdef ENABLE_DBG
@@ -62,20 +74,21 @@ uint8_t DFRobot_Sensor::switchMode(uint8_t mode)
     return
   */
 }
-void DFRobot_Sensor::setLED(uint8_t r, uint8_t g, uint8_t b)
+
+void DFRobot_ZXL::setLED(uint8_t r, uint8_t g, uint8_t b)
 {
-  sColor_t data={.b=b>>3,.g=g>>2,.r=r>>3};
+  sColor_t data={b>>3,g>>2,r>>3};//
   writeReg(SENSOR_ADDR_LED, &data, sizeof(data));
 }
 
-void DFRobot_Sensor::setLED(uint16_t color)
+void DFRobot_ZXL::setLED(uint16_t color)
 {
   writeReg(SENSOR_ADDR_LED, &color, sizeof(color));
 }
 
 
-DFRobot_Sensor_IIC::DFRobot_Sensor_IIC(TwoWire *pWire, uint8_t mode)
-  :DFRobot_Sensor(mode)  //初始化列表：子类用父类给的构造函数（就是上一个框里的函数）从而初始化了父类的私有变量_mode
+DFRobot_ZXL_IIC::DFRobot_ZXL_IIC(TwoWire *pWire, uint8_t mode)
+  :DFRobot_ZXL(mode)  //初始化列表：子类用父类给的构造函数（就是上一个框里的函数）从而初始化了父类的私有变量_mode
 {
   _deviceAddr = DFRobot_Sensor_IIC_ADDR;        //因为用到了宏定义，所以单独注释说明，源码的.cpp是没有这个注释的
   _pWire = pWire;     //子类构造函数给自己的成员赋值，什么意思呢，下面是涉及的宏定义
@@ -85,18 +98,25 @@ DFRobot_Sensor_IIC::DFRobot_Sensor_IIC(TwoWire *pWire, uint8_t mode)
 
 
 
-int DFRobot_Sensor_IIC::begin(void)
+int DFRobot_ZXL_IIC::begin(void)
 {
   Wire.begin();   //Wire.h（IIC）库函数 初始化wire库
-  return DFRobot_Sensor::begin();  //执行上面的初始化函数
+  return DFRobot_ZXL::begin();  //执行上面的初始化函数
 }
 
+/* int DFRobot_Sensor_IIC::begin(void)
+{
+  Wire.begin();   //Wire.h（IIC）库函数 初始化wire库
+  int ret = DFRobot_Sensor::begin();  //执行上面的初始化函数
+  return ret;
+} */
 
 
-void DFRobot_Sensor_IIC::writeReg(uint8_t reg, void* pBuf, size_t size)
+
+void DFRobot_ZXL_IIC::writeReg(uint8_t reg, void* pBuf, size_t size)//具体过程
 {
   if(pBuf == NULL){
-          DBG("pBuf ERROR!! : null pointer");
+          DBG("pBuf ERROR!! : null pointer");//一个判断能增加调试能力，提示输入错误
   }
   uint8_t * _pBuf = (uint8_t *)pBuf;
   _pWire->beginTransmission(_deviceAddr);//芯片地址DFRobot_Sensor_I2C_ADDR 0x66
@@ -108,12 +128,12 @@ void DFRobot_Sensor_IIC::writeReg(uint8_t reg, void* pBuf, size_t size)
   _pWire->endTransmission();
 }
 
-uint8_t DFRobot_Sensor_IIC::readReg(uint8_t reg, void* pBuf, size_t size)
+uint8_t DFRobot_ZXL_IIC::readReg(uint8_t reg, void* pBuf, size_t size)
 {
   if(pBuf == NULL){
     DBG("pBuf ERROR!! : null pointer");
   }
-  uint8_t * _pBuf =  (uint8_t*)pBuf; //传进来的是buf的地址，通过指针修改buf的内容//_pBuf定义在wire里的？
+  uint8_t * _pBuf =  (uint8_t*)pBuf; //传进来的是buf的地址，通过指针修改buf的内容//
   _pWire->beginTransmission(_deviceAddr);
    _pWire->write(& reg, 1);                         //用到了Wire.write()
   if( _pWire->endTransmission() != 0){                 //用到了 Wire.endTransmission()，结束一个由beginTransmission（）开始的并且由write（）排列的从机的传输。返回0 成功 1 数据溢出 2 发送错误或其他错误
@@ -125,22 +145,29 @@ uint8_t DFRobot_Sensor_IIC::readReg(uint8_t reg, void* pBuf, size_t size)
     _pBuf[i] = _pWire->read();        //用read()接收放到buf里
   }
   _pWire->endTransmission();
-  return size;
+  return size;//
 }
 
-//SPI部分待完善
-int DFRobot_Sensor_SPI::begin(void)
+//SPI部分
+DFRobot_ZXL_SPI::DFRobot_ZXL_SPI(SPIClass *pSpi, uint8_t csPin, uint8_t mode)
+  :DFRobot_ZXL(mode)
+{
+  _pSpi = pSpi;
+  _csPin = csPin;
+}
+
+int DFRobot_ZXL_SPI::begin(void)
 {
   pinMode(_csPin, OUTPUT);
   _pSpi->begin();
-  return DFRobot_Sensor::begin();
+  return DFRobot_ZXL::begin();
 }
 
-void DFRobot_Sensor_SPI::writeReg(uint8_t reg, void* pBuf, size_t size)
+void DFRobot_ZXL_SPI::writeReg(uint8_t reg, void* pBuf, size_t size)
 {
   if(pBuf == NULL){
-	  DBG("pBuf ERROR!! : null pointer");
-  }
+  DBG("pBuf ERROR!! : null pointer");
+}
   uint8_t * _pBuf = (uint8_t *)pBuf;
   _pSpi->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
   digitalWrite(_csPin, 0);
@@ -153,10 +180,10 @@ void DFRobot_Sensor_SPI::writeReg(uint8_t reg, void* pBuf, size_t size)
   SPI.endTransaction();
 }
 
-uint8_t DFRobot_Sensor_SPI::readReg(uint8_t reg, void* pBuf, size_t size)
+uint8_t DFRobot_ZXL_SPI::readReg(uint8_t reg, void* pBuf, size_t size)
 {
   if(pBuf == NULL){
-	  DBG("pBuf ERROR!! : null pointer");
+    DBG("pBuf ERROR!! : null pointer");
   }
   uint8_t * _pBuf = (uint8_t *)pBuf;
   size_t count = 0;
--- DFRobot_ZXL.h
+++ DFRobot_ZXL.h
@@ -8,13 +8,13 @@
 
 #define ENABLE_DBG  1       //注释掉这句就可省略运行过程，不打印。把1改成0也可以不打印
 #ifdef ENABLE_DBG
-#define DBG(...) {Serial.print("[");Serial.print(__FUNCTION__); Serial.print("(): "); Serial.print(__LINE__); Serial.print(" ] "); Serial.println(__VA_ARGS__);}
+#define DBG(...) {Serial.print("[");Serial.print(__FUNCTION__); Serial.print("(): "); Serial.print(__LINE__); Serial.print(" ] "); Serial.println(__VA_ARGS__);}//可以传参，方便打印调试信息
 #else
-#define DBG(...)//？
+#define DBG(...)/...宏的可变参
 #endif
 
 #define DFRobot_Sensor_IIC_ADDR 0x66 //芯片IIC地址，无变化地址功能
-#define DFRobot_Sensor_ID 0xDF //芯片IIC地址，无变化地址功能
+#define DFRobot_Sensor_ID 0xDF //芯片ID
 
 #define SENSOR_ADDR_LED    0 //LED控制地址  这里的描述从芯片手册上抄写
 #define SENSOR_ADDR_DATA   2 //组合数据地址  这里的描述从芯片手册上抄写
@@ -43,7 +43,7 @@
 
 
 
-class DFRobot_Sensor
+class DFRobot_ZXL//
 {
 public:
 
@@ -52,70 +52,59 @@ public:
 #define ERR_IC_VERSION    -2      ///< 芯片版本不匹配
 
 
-	typedef struct {
-		uint8_t   light: 4;
-		uint8_t   sound: 4;
-	  }__attribute__ ((packed)) sCombinedData_t;
-	  
-	typedef struct {
-		uint8_t   b: 5;
-		uint8_t   g: 6;
-		uint8_t   r: 5;
-	  } __attribute__ ((packed)) sColor_t;
-	  
-	typedef enum{
+  typedef struct {
+    uint8_t   light: 4;
+    uint8_t   sound: 4;
+  }__attribute__ ((packed)) sCombinedData_t;//占用一个字节，但是怎么放的？谁在高4位？
+    
+  typedef struct {
+    uint8_t   b: 5;
+    uint8_t   g: 6;
+    uint8_t   r: 5;
+  } __attribute__ ((packed)) sColor_t;//说明是此类的结构体，防止混淆
+    
+  typedef enum{
     eNormalPower = 0, /**< 正常功耗模式，功耗范围20mW-60mW，可以搭配任意采集速度eSpeedMode_t和采集精度ePrecisionMode_t */
     eLowPower = 1, /**< 低功耗模式，功耗范围2mW-4mW, 注意在低功耗模式下，采集速度eSpeedMode_t只能搭配eNormalSpeed，采集精度ePrecisionMode_t只能搭配eLowPrecision和eNomalPrecision*/
   }eLowPowerMode_t;
   
-  /**
-   * @enum eSpeedMode_t
-   * @brief 简述枚举用途
-   * @details 提示一些必要的技术细节(从数据手册上抄写)
-   * @note 注解
-   * @attention 注意事项
-   */
+  
   typedef enum{
     eNormalSpeed = 0<<1, /**< 正常采集速度，可以和任意精度搭配使用 */
     eHighSpeed = 1<<1,   /**< 高速采集模式，采集周期10ms，可以进入低功耗，可以配置为eLowPrecision和eNomalPrecision两种精度模式 */
   }eSpeedMode_t;
   
-  /**
-   * @enum ePrecisionMode_t
-   * @brief 简述枚举用途
-   * @details 提示一些必要的技术细节(从数据手册上抄写)
-   * @note 注解
-   * @attention 注意事项
-   */
+  
   typedef enum{
     eLowPrecision   = 0<<2, /**< 低精度，精度大概在xxx，在低精度模式下，可以进入低功耗 */
     eNomalPrecision = 1<<2, /**< 正常精度，精度大概在xxx，在正常精度模式下，可以进入低功耗 */
     eHighPrecision  = 2<<2, /**< 高精度，精度大概在xxx，在高精度模式下，采集速率会降低，采集周期100ms，不能进入低功耗 */
     eUltraPrecision = 3<<2, /**< 超高精度，精度大概在xxx，在超高精度模式下，采集速率会极低，采集周期1000ms，不能进入低功耗 */
   }ePrecisionMode_t;
-  DFRobot_Sensor(uint8_t mode);
+  
+  DFRobot_ZXL(uint8_t mode);
   virtual int begin(void); //子类的初始化方法不同，父类不会去实现这个函数，加上virtual声明为虚函数，交给子类去实现
   uint16_t soundStrengthDB(void);
   uint32_t lightStrengthLux(void);
   uint8_t switchMode(uint8_t mode);
   void setLED(uint8_t r, uint8_t g, uint8_t b);
   void setLED(uint16_t color);
+
 protected:
   virtual void writeReg(uint8_t reg, void* pBuf, size_t size) = 0;  //父类声明纯虚函数是想让子类实现，来兼容各种接口的芯片，例如IIC、SPI。
   virtual uint8_t readReg(uint8_t reg, void* pBuf, size_t size) = 0;
-  private:
-	uint8_t _mode;
-	TwoWire *_pWire;
-	
+
+private:
+  uint8_t _mode;
 };
 
 
 
-class DFRobot_Sensor_IIC:public DFRobot_Sensor
+class DFRobot_ZXL_IIC:public DFRobot_ZXL
 {
 public:
-  DFRobot_Sensor_IIC(TwoWire *pWire=&Wire, uint8_t mode=eNomalPrecision+eNormalSpeed+eNormalPower);  //子类的构造函数里加上了默认参数,mode=精度、采样速率、功耗。*pWire=&Wire？
-  virtual int begin(void);  //与众不同的功能，在IIC子类中这是初始化IIC//为啥要虚构函数？
+  DFRobot_ZXL_IIC(TwoWire *pWire=&Wire, uint8_t mode=eNomalPrecision+eNormalSpeed+eNormalPower);  //子类的构造函数里加上了默认参数,mode=精度、采样速率、功耗。*pWire=&Wire？
+  virtual int begin(void);  //与众不同的功能，在IIC子类中这是初始化IIC
 protected:
    virtual void writeReg(uint8_t reg, void* pBuf, size_t size);  //这是通过IIC写寄存器
   virtual uint8_t readReg(uint8_t reg, void* pBuf, size_t size);  //这是通过IIC读寄存器
@@ -124,17 +113,17 @@ protected:
   TwoWire *_pWire;
 };
 
-class DFRobot_Sensor_SPI:public DFRobot_Sensor
+class DFRobot_ZXL_SPI:public DFRobot_ZXL
 {
-	public:
-  DFRobot_Sensor_SPI(SPIClass *spi=&SPI, uint8_t csPin=4, uint8_t mode=eNomalPrecision+eNormalSpeed+eNormalPower);  //SPI.h里定义了extern SPIClass SPI;因此取SPI对象的地址就能够指向并使用SPI中的方法  //第二个参数csPin是指定cs接的数字引脚
+public:
+  DFRobot_ZXL_SPI(SPIClass *spi=&SPI, uint8_t csPin=4, uint8_t mode=eNomalPrecision+eNormalSpeed+eNormalPower);  //SPI.h里定义了extern SPIClass SPI;因此取SPI对象的地址就能够指向并使用SPI中的方法  //第二个参数csPin是指定cs接的数字引脚
   virtual int begin(void);
 protected:
-  virtual void writeReg(uint8_t reg, void* pBuf, size_t size) = 0;//为啥写纯虚构函数？
-  virtual uint8_t readReg(uint8_t reg, void* pBuf, size_t size) = 0;
+  virtual void writeReg(uint8_t reg, void* pBuf, size_t size) ;
+  virtual uint8_t readReg(uint8_t reg, void* pBuf, size_t size) ;
   private:
   SPIClass *_pSpi;
   uint8_t _csPin; 
-	
+
 };
 #endif
